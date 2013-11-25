8d82ba3df6fa5ff12ed845f1893cf628e00d032d
Wes Hayutin <whayutin@redhat.com>
2013-11-25 14:28:35 -0500

    remove tmp dir when finished

--- sync_jenkins.sh
+++ sync_jenkins.sh
@@ -1,6 +1,6 @@
 HOST=$1; shift
 REMOTE=$1
-LOG=/tmp/sync_jenkins
+LOG=$HOME/sync_jenkins.log
 TMP=/tmp/jenkins_sync
 
 if [ ! -d $TMP ];then
@@ -14,15 +14,17 @@ JOBS="packstack-rdo-havana-rhel-nova-production packstack-rdo-havana-neutron-rhe
 
 echo "**** START RDO JENKINS SYNC ****"  >>  $LOG
 for job in `echo $JOBS`; do
-  rsync -auvz -L -e "ssh -i /home/whayutin/.ssh/JENKINS/id_rsa" root@$HOST:/var/lib/jenkins/jobs/$job $TMP/ &>> $LOG
+  rsync -auvz -L -e "ssh -i $HOME/.ssh/JENKINS/id_rsa" root@$HOST:/var/lib/jenkins/jobs/$job $TMP/ &>> $LOG
 done
 echo "**** END LOCAL SYNC ****"  >> $LOG
 echo "**** BEGIN REMOTE SYNC ****"  >> $LOG
 for job in `echo $JOBS`; do
-  rsync -auvz -L -e "ssh -i /home/whayutin/.ssh/JENKINS/id_rsa" $TMP/$job 524ee18d4382ec1886000084@$REMOTE:~/app-root/data/jobs/ &>> $LOG
+  rsync -auvz -L -e "ssh -i $HOME/.ssh/JENKINS/id_rsa" $TMP/$job 524ee18d4382ec1886000084@$REMOTE:~/app-root/data/jobs/ &>> $LOG
 done
 echo "**** END REMOTE SYNC ****"  >> $LOG
 
 #restart
 curl -X POST --show-error https://admin:0c3b58082e6426a6583b64203bfe47c2@prod-rdojenkins.rhcloud.com/restart &>> $LOG
+
+rm -Rf $TMP
 
